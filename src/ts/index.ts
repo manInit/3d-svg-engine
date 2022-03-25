0505943c512b58e9debd3b0cd5b1ee94357008fa
manInit <info.denisbronnikov@gmail.com>
2022-03-26 01:35:47 +0800

    Объекта пола, и демонстрация проблемы с отсечением

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,16 +4,22 @@ import Camera from './core/Camera'
 import MapWorld from './MapWorld'
 import World from './World'
 import Cube from './worldObject/Cube'
+import Floor from './worldObject/Floor'
+import ObjectWorld from './worldObject/ObjectWorld'
 import Pyramid from './worldObject/Pyramid'
 
 const cube = new Cube(100, 'black', 0, 0, 0)
 const p = new Pyramid(100, 'red', 0, 0, 200)
 const p1 = new Pyramid(300, 'yellow', 300, 0, -100)
 const p2 = new Pyramid(250, 'green', -1000, 0, 350)
+const floor = new Floor(1000, 30, 'blue', -400, -400, -400)
+
+
+
 const camera = Camera.getInstance(window.innerWidth)
 camera.position = {x: -100, y: 0, z: -200}
 
-const objects = [...cube.sides, ...p.sides, ...p1.sides, ...p2.sides]
+const objects = [...cube.sides, ...p.sides, ...p1.sides, ...p2.sides, ...floor.sides]
 
 const world = new World('world', window.innerWidth, window.innerHeight, objects)
 const map = new MapWorld('map', 200, 200, world)
---
+++ src/ts/worldObject/Floor.ts
@@ -0,0 +1,33 @@
+import Transform from '../core/Transform'
+import ObjectWorld from './ObjectWorld'
+
+export default class Floor {
+  public sides: ObjectWorld[]
+
+  constructor(size: number, depth: number, color: string, x: number, y: number, z: number) {
+
+    this.sides = [
+      new ObjectWorld([
+        { x: -size, y: -depth, z: -size },
+        { x: size, y: -depth, z: -size },
+        { x: size, y: -depth, z: size },
+        { x: -size, y: -depth, z: size }
+      ])
+    ]
+
+    for (const obj of this.sides) {
+      obj.color = color
+    }
+
+    this.translate(x, y, z)
+  }
+
+  public translate(x: number, y: number, z: number) {
+    for (const obj of this.sides) {
+      for (let i = 0; i < obj.points.length; i++) {
+        obj.points[i] = Transform.translate(obj.points[i], x, y, z)
+      }
+    }
+  }
+
+}
