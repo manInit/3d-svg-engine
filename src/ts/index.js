953c67fa7e23dd0642ee03f3c46eafbdf89fa57e
manInit <info.denisbronnikov@gmail.com>
2022-05-16 02:00:18 +0800

    add user functions for update

--- src/ts/index.js
+++ src/ts/index.js
@@ -1,9 +1,18 @@
 import './engine.ts'
 
-add(sphere(10, 100, 100, 0, 'red'))
-add(cube(10, 100, 100, 100, 'red'))
-add(pyramid(10, 30, 30, 30, 'red'))
-add(parallelepiped(10, 30, 30, 30, 100, 200, 'red'))
+const sphere1 = sphere(10, 100, 100, 0, 'red')
+const pyramid1 = pyramid(10, 0, 0, 0, 'red')
+const cube1 = cube(10, 100, 100, 100, 'red')
+const x = parallelepiped(10, 30, 30, 30, 100, 200, 'red')
+add(sphere1, cube1, x, pyramid1)
+
+let ax = 0
+update(() => {
+  sphere1.translate(1, 1, 0)
+  cube1.rotate(1, 0, 0)
+  x.translate(Math.sin(ax), Math.sin(ax), 0)
+  ax += 0.1
+})
 // const world = new World(document.getElementById('world'))
 // world.addBgElem(new BackgroundElem('../textures/seraphim.png', 200, 100))
 // world.addBgElem(new BackgroundElem('../textures/seraphim.png', 400, 100))
