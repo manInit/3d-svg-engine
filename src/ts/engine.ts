40fef22546d0350b651aac9285385c79932c8aa0
manInit <info.denisbronnikov@gmail.com>
2022-05-23 21:39:21 +0800

    create separate class for engine

--- src/ts/engine.ts
+++ src/ts/engine.ts
@@ -7,62 +7,49 @@ import Pyramid from './ObjectsWorld/Pyramid'
 import Sphere from './ObjectsWorld/Sphere'
 import Floor from './ObjectsWorld/Floor'
 import Camera from './core/Camera'
-import Square from './ObjectsWorld/Square'
 
-declare global {
-  interface Window { 
-    player: Camera
+class SVGEngine {
+  private world: World
+  public player: Camera
 
-    cube: (size: number, x: number, y: number, z: number, color: string) => Cube
-    pyramid: (size: number, x: number, y: number, z: number, color: string) => Pyramid
-    parallelepiped: (sizea: number, sizeb: number, sizec: number, x: number, y: number, z: number, color: string) => Parallelepiped
-    sphere: (r: number, x: number, y: number, z: number, color: string) => Sphere
+  constructor(idWorld: string) {
+    const elem = document.getElementById(idWorld)
+    this.world = new World(elem)
+    this.player = this.world.cameraObj
+    this.world.run(120)
+  }
+
+  cube = (size: number, x = 0, y = 0, z = 0, color = 'black') => new Cube(size, {x, y, z}, color)
+  pyramid = (size: number, x = 0, y = 0, z = 0, color = 'black') => new Pyramid(size, {x, y, z}, color)
+  parallelepiped = (sizea: number, sizeb: number, sizec: number, x = 0, y = 0, z = 0, color = 'black') => new Parallelepiped(sizea, sizeb, sizec, {x, y, z}, color)
+  sphere = (r: number, x = 0, y = 0, z = 0, color = 'black') => new Sphere({x, y, z}, r, color)
+  floor = (size: number, x = 0, y = 0, z = 0, color = 'black') => new Floor(size, {x, y, z}, color)
+
+  setBackground = (urlImage: string) => this.world.setBackground(urlImage)
+  add = (...obj: ObjectWorld[]) => this.world.addObjects(...obj)
+  update = (cb: () => void) => this.world.setUpdateFunction(cb)
+  
+  saveScreen = () => {
+    const svgRoot = this.world.svgRootElement
+    const serializer = new XMLSerializer()
+  
+    let source = serializer.serializeToString(svgRoot)
+    source = '<?xml version="1.0" standalone="no"?>\r\n' + source
     
-    add: (...obj: ObjectWorld[]) => void
-    update: (cb: () => void) => void
-    setBackground: (urlImage: string) => void
-    saveScreen: () => void
-
-    floor: (size: number, x: number, y: number, z: number, color: string) => Floor
-    square: (size: number, x: number, y: number, z: number, color: string) => Square
+    const url = 'data:image/svg+xml;charset=utf-8,'+encodeURIComponent(source)
+    const a = document.createElement('a')
+    const e = new MouseEvent('click')
+  
+    a.download = 'download.svg'
+    a.href = url
+    a.dispatchEvent(e)
   }
 }
 
-let rootElem = document.getElementById('world')
-if (!rootElem) {
-  rootElem = document.createElement('div')
-  rootElem.id = 'world'
-  document.body.append(rootElem)
-}
-const world = new World(rootElem)
-
-window.cube = (size: number, x = 0, y = 0, z = 0, color = 'black') => new Cube(size, {x, y, z}, color)
-window.pyramid = (size: number, x = 0, y = 0, z = 0, color = 'black') => new Pyramid(size, {x, y, z}, color)
-window.parallelepiped = (sizea: number, sizeb: number, sizec: number, x = 0, y = 0, z = 0, color = 'black') => new Parallelepiped(sizea, sizeb, sizec, {x, y, z}, color)
-window.sphere = (r: number, x = 0, y = 0, z = 0, color = 'black') => new Sphere({x, y, z}, r, color)
-window.floor = (size: number, x = 0, y = 0, z = 0, color = 'black') => new Floor(size, {x, y, z}, color)
-
-window.setBackground = (urlImage: string) => world.setBackground(urlImage)
-window.add = (...obj: ObjectWorld[]) => world.addObjects(...obj)
-window.update = (cb: () => void) => world.setUpdateFunction(cb)
-window.player = world.cameraObj
-
-window.saveScreen = () => {
-  const svgRoot = world.svgRootElement
-  const serializer = new XMLSerializer()
-
-  let source = serializer.serializeToString(svgRoot)
-  source = '<?xml version="1.0" standalone="no"?>\r\n' + source
-  
-  const url = 'data:image/svg+xml;charset=utf-8,'+encodeURIComponent(source)
-  const a = document.createElement('a')
-  const e = new MouseEvent('click')
-
-  a.download = 'download.svg'
-  a.href = url
-  a.dispatchEvent(e)
+declare global {
+  interface Window { 
+    SVGEngine: (id: string) => SVGEngine
+  }
 }
 
-world.run(120)
-
-
+window.SVGEngine = (id: string) => new SVGEngine(id)
--- src/ts/index.js
+++ src/ts/index.js
@@ -1,1 +1,5 @@
 import './engine.ts'
+
+const engine = SVGEngine('world')
+const cube = engine.cube(100)
+engine.add(cube)
